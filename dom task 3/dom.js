a8c21699fb548d799c9bb80bc65533612f3b331a
Suresh-Kumar-Lohar <sureshlohar468@gmail.com>
2022-10-23 12:14:04 +0530

    new2

---
+++ dom task 3/dom.js
@@ -0,0 +1,40 @@
+// EXAMINE THE DOCUMENT OBJECT //
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// //document.title =  123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// GETELEMENTBYID //
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header')
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.border = 'solid 3px black'
+
+var body_title = document.getElementById('main-title')
+console.log(body_title)
+body_title.style.fontWeight = 'bold'
+body_title.style.color = 'green'
+
+// GETELEMENTSBYCLASSNAME //
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
